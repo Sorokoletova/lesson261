64a480cd3dbf9339f39c7ec0169d01c3109cc3c6
sorokletova <sorokoletova_t@inbox.ru>
2022-07-05 13:02:54 +0300

    deploy7

--- config-ci.py
+++ config-ci.py
@@ -15,6 +15,6 @@ class TestingConfig(BaseConfig):
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://$USER_DB:$PASSWORD_DB@pg/$NAME_DB"
+    SQLALCHEMY_DATABASE_URI = "postgresql://$DB_USER:$DB_PASSWORD@pg/$DB_NAME"
 #        "sqlite:///project.db"
 #       "postgresql://user_db:password@db/app_db"
